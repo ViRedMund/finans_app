0f061b715db72fcb478131ceaacff224b0ea7483
RedMund <BlackRedMund@mail.ru>
2026-08-24 23:22:14 +0300

    две простые ручки на создание кошелька и добавление баланса и проверка баланса кошелька

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from fastapi import FastAPI
-from fastapi.responses import Response
+from fastapi import FastAPI, HTTPException
 
 
 #Инициализация FastAPI проекта
 app = FastAPI()
 
 
-@app.get("/health")
-def health_cheack():
-    return Response(status_code=200)
+# Словарь для хранение баланса
+# Ключ - название кошелька, значение - баланс кошелька
+BALANCE = {}
+
+
+@app.get("/balance")
+def get_balance(wallet_name: str | None = None):
+    # Если имя кошелька не указанно, считаем общий баланс
+    if wallet_name is None:
+        return {"total_balance": sum(BALANCE.values())}
+
+    # Проверяем существует ли запрашиваемый кошелёк 
+    if wallet_name not in BALANCE:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Wallet '{wallet_name}' not found"
+        )
+
+    # Возврощаем баланс конкретного кошелька
+    return {"Wallet": wallet_name, "balance": BALANCE[wallet_name]}
+
+@app.post("/wallets/{name}")
+def receive_mony(name: str, amount: int):
+    # Если кошелька с таким именем нет, создаем его с балансом 0
+    if name not in BALANCE:
+        BALANCE[name] = 0
+
+    # Добавляем сумму к балансу кошелька
+    BALANCE[name] += amount
+
+    # Возвращаем информацию об операции
+    return {
+        "massege": f"Added {amount} to {name}",
+        "wallet": name,
+        "new_balance": BALANCE[name]
+    }
